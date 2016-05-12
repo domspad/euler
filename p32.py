3f321782113e11867ad64aa750cb85ad516f548c
domspad <domspad@umich.edu>
2016-05-12 16:30:05 -0500

    21

---
+++ p32.py
@@ -0,0 +1,27 @@
+
+
+# 15 mins restricted brute
+# In [24]: %time %run p32.py
+# 45228
+# CPU times: user 751 ms, sys: 41.8 ms, total: 793 ms
+# Wall time: 788 ms
+
+prods = []
+
+def is_pan_dig(a,b):
+	ab = a*b
+	return ''.join(sorted(''.join(map(str, (a,b,ab))))) == '123456789'
+
+for a in xrange(10):
+	for b in xrange(10**4):
+		if is_pan_dig(a,b):
+			print a, b, a*b
+			prods.append(a*b)
+
+for a in xrange(100):
+	for b in xrange(1000):
+		if is_pan_dig(a,b):
+			print a, b, a*b
+			prods.append(a*b)
+
+print sum(set(prods))
